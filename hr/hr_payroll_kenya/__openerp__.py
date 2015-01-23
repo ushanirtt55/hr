4b2ff5d803f56afe1a9d804e3fc21ab6a3e4aa52
ushanirtt55 <ushanirtt55@gmail.com>
2015-01-23 17:58:43 +0530

    View_1

--- hr/hr_payroll_kenya/__openerp__.py
+++ hr/hr_payroll_kenya/__openerp__.py
@@ -1,11 +1,11 @@
 {
     'name' : 'Payroll Kenya',
     'version': '1.0',
-    'author': 'Mitosis Tech',
-    'description': "My first sample odoo-8 module",
+    'author': 'Mitosis Technologies',
+    'description': "Payroll customization for Kenya",
     'website':'www.mitosistech.com',
     'category':'Localization',
-    'depends': ['hr','l10n_in_hr_payroll'],
+    'depends': ['base','hr','l10n_in_hr_payroll','hr_payroll'],
     'data': ['view_2.xml','view_1.xml'],
     'installable': True,
 }
